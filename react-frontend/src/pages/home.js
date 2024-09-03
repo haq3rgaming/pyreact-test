7baec9c0c816e45b86ec74460337e8c52c04e570
Patrik Puskar <puskarpato@gmail.com>
2024-09-03 14:45:34 +0200

    edit home

--- react-frontend/src/pages/home.js
+++ react-frontend/src/pages/home.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 import Message from "../components/Message";
-import { CookiesProvider, useCookies } from 'react-cookie'
 
 function Home() {
 
